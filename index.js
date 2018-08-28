e2137e09cd3b27ef36a13df27a65eecf261e1ba1
Gage Henderson <gage@quangoinc.com>
2018-08-28 11:08:51 -0700

    It should now add a nose to the body of the pageg

---
+++ index.js
@@ -0,0 +1,9 @@
+// http://www.stickpng.com/assets/images/590602570cbeef0acff9a633.png
+
+function createNoses() {
+  $('body').append(
+    '<img src="http://www.stickpng.com/assets/images/590602570cbeef0acff9a633.png" />'
+  );
+}
+
+module.exports = createNoses;
